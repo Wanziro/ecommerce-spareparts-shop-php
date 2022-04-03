a9dd5e1042b1e568ca823a524949d1e615173671
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-04-03 08:09:20 +0200

    added seo metadata

--- main_header.php
+++ main_header.php
@@ -1,9 +1,24 @@
 <meta charset="utf-8" />
 <meta http-equiv="x-ua-compatible" content="ie=edge" />
 <title>AUTO Experts Rwanda</title>
-<meta name="description" content="" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="shortcut icon" href="logo/logo.jpg" type="image/jpg">
+<meta name="theme-color" content="#4285f4">
+<meta name="keywords" content="AUTO Experts Rwanda, AUTO Experts, Garages, spare parts, online shopping, car renting in Rwanda, car renting,car shopping">
+<meta name="description" content="AUTO Experts Rwanda is a leading East Africa's Automotive based service provider based in Kigali Rwanda. The company provide services and products to customer level in order to satisfy growing automotive industry in Rwanda and Africa.">
+
+<!-- FACEBOOK METADATA -->
+<meta property="og:url" content="https://autoexpertsrwanda.com">
+<meta property="og:type" content="website">
+<meta property="og:image" content="https://autoexpertsrwanda.com/images/logo/logo.jpg">
+<meta property="og:description" content="AUTO Experts Rwanda is a leading East Africa's Automotive based service provider based in Kigali Rwanda. The company provide services and products to customer level in order to satisfy growing automotive industry in Rwanda and Africa.">
+<meta property="og:title" content="AUTO Experts Rwanda">
+
+<!-- TWITTER METADATA -->
+<meta name="twitter:title" content="AUTO Experts Rwanda">
+<meta name="twitter:description" content="AUTO Experts Rwanda is a leading East Africa's Automotive based service provider based in Kigali Rwanda. The company provide services and products to customer level in order to satisfy growing automotive industry in Rwanda and Africa.">
+<meta name="twitter:image" content="https://autoexpertsrwanda.com/images/logo/logo.jpg">
+
 
 <link rel="stylesheet" href="css/material-design-iconic-font.min.css" />
 <link rel="stylesheet" href="css/font-awesome.min.css" />
